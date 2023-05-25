345419beee87b2d2bfd5cbacbf8934bbc4260d9b
wendell1108 <wendell.pg11@gmail.com>
2023-05-25 09:48:42 -0300

    Update ex012.js
    relembrando uams coisas simples q podem confudir a gente

--- exercicios/aula 12/ex012.js
+++ exercicios/aula 12/ex012.js
@@ -1,8 +1,9 @@
 var Agora=new Date()
 var hora= Agora.getHours()
+let nome = 'wendell'
 console.log(`Agora são exatamente ${hora} horas.`)
 if(hora<12){
-    console.log('Bom dia!')
+    console.log(`Bom dia! ${nome}`)
 } else if(hora<=18){
     console.log('Boa tarde!')
 } else{
